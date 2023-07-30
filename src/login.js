f4ceaefd9c7492f8653d489d260c1775de14f9dc
Sammany1 <Sammany1@users.noreply.github.com>
2023-07-30 15:24:22 +0300

    update

--- src/login.js
+++ src/login.js
@@ -7,17 +7,32 @@ export default function Login() {
         
         <div className="login">
             <div className="background" >
-                <div className="box">
-                    <div className="box_text">
+                <div className="left-box">
+                    <div className="box_text_left">
                     Please Enter Room ID Here
                     </div>
-                    <div className="box_input">
+                    <div className="box_input_left">
                     <form action="/url" method="GET">
                     <input type="text" id="id" name="room_id" placeholder="Enter room ID.."></input>
                     </form>
                     </div>
                     <div >
-                    <button className="button" type="button" id="myBtn" onclick="Enter_Button()" >Enter</button>
+                    <button className="button_left" type="button" id="myBtn" onclick="Enter_Button()" >Enter</button>
+                    {/* <input type="button" class="button" value="Enter"></input> */}
+                    {/* <button class="button-50" role="button">Button 50</button> */}
+                    </div>
+                    </div>
+                    <div className="right-box">
+                    <div className="box_text_right">
+                    Please Enter Room ID Here
+                    </div>
+                    <div className="box_input_right">
+                    <form action="/url" method="GET">
+                    <input type="text" id="id" name="room_id" placeholder="Enter room ID.."></input>
+                    </form>
+                    </div>
+                    <div >
+                    <button className="button_right" type="button" id="myBtn" onclick="Enter_Button()" >Enter</button>
                     {/* <input type="button" class="button" value="Enter"></input> */}
                     {/* <button class="button-50" role="button">Button 50</button> */}
                     </div>
