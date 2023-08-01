afe2f00f8d7df8430a040e1e5dc4f994d12d85b5
Sammany1 <Sammany1@users.noreply.github.com>
2023-08-01 21:52:57 +0300

    card and owned updateee

--- src/card.js
+++ src/card.js
@@ -12,7 +12,8 @@ export default function Card() {
     // console.log(data)    
     
     return (
-        <div className="card">
+    <div>
+        <div className="profile_card">
             <div className="left">
             <div className="top_text">
             <div className="top_text_header" >
@@ -20,25 +21,59 @@ export default function Card() {
             </div>
             <div className="top_text_sub">
                 your account value is
-                
             </div>
             </div>
-             
             <div className="main_number">
-            ${sample.users.random_id} 
+                ${sample.users.random_id} 
             </div>
             </div>
             <div className="right_text"> 
-            
                 Available Amount = $240 <br/>
                 Stocks Value = $1090.29 <br/> 
                 Profit = + $240
-
             </div>
-                
-                
-               
-            
         </div>
+
+
+        <div className="owned_card">
+            <div className="row">
+                <div className="name">
+                    MSFT
+                </div> 
+                <div className="status">
+                    +9.77%
+                </div>
+                <div className="price">
+                    $681.79
+                </div>
+                <div className="owned">
+                    $10000$
+                </div>
+                
+            </div>
+            <div className="row">
+                <div className="name">
+                    AMZN
+                </div> 
+                <div className="status">
+                    +9.77%
+                </div>
+                <div className="price">
+                    $681.79
+                </div>
+                <div className="owned">
+                    $10000$
+                </div>
+                
+            </div>
+           
+           
+           {/* AMZN <br/>
+            AAPL <br/> 
+            IBM  <br/>
+  */}
+
+        </div>
+    </div>
     );
 }
